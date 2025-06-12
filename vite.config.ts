d072bfedaaeae3c9543acdbc8cc9bf354a62151f
Preston M. <157419875+airvair@users.noreply.github.com>
2025-06-12 06:31:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  // 👇 change this to the folder you chose in the Deployments panel
+  //     KEEP the leading and trailing slashes.
+  base: mode === "development" ? "/" : "/wp-react-app/",
+
+  plugins: [react()],
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
-});
+      "@": path.resolve(__dirname, "."),
+    },
+  },
+}));
