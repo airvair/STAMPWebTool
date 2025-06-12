e611b8822a80cc61c49dc288ffa26fcc1220899d
Preston M. <157419875+airvair@users.noreply.github.com>
2025-06-12 06:43:19 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";   // <-- make sure this is installed
 
 export default defineConfig(({ mode }) => ({
   // return value ⬆ is now an **object expression**, not a function body
-  base: mode === "development" ? "/" : "/wp-react-app/",
+  base: mode === "development" ? "/" : "/STAMPWebTool/",
 
   plugins: [react()],
 
