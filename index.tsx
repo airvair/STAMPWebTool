60f6a8b46aafd644013c13ecaacb8a4f990f9dfd
Preston M <157419875+airvair@users.noreply.github.com>
2025-07-12 13:56:53 -0500

    gh fix

--- index.tsx
+++ index.tsx
@@ -15,7 +15,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ProjectsProvider>
         <AnalysisProvider>
           <App />
